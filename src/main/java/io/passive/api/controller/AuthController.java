e935b0cdbce4f92c6b7541bfabf3c167c84e71d1
MINOHKWON <alsdh79@gmail.com>
2023-01-22 15:44:44 +0900

    20220122.KMO.MOD - 설정 변경 완료

---
+++ src/main/java/io/passive/api/controller/AuthController.java
@@ -0,0 +1,19 @@
+package io.passive.api.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 로그인 Controller
+ *
+ * @author : KMO
+ * @fileName : AuthController.java
+ * @since : 2022.01.22
+ */
+@RestController
+@Api(value = "AuthController")
+@RequestMapping(value = "/api")
+@CrossOrigin(origins = "*")
+public class AuthController {
+
+}
